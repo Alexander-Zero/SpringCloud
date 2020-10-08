774428cc7c8c83e9f01504097dcf2e4dbaf54b89
Alexander-Zero <51397937+Alexander-Zero@users.noreply.github.com>
2020-10-09 00:27:03 +0800

    Initial commit

---
+++ Service-B/src/main/java/com/alex/zero/serviceb/ServiceBApplication.java
@@ -0,0 +1,13 @@
+package com.alex.zero.serviceb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceBApplication.class, args);
+	}
+
+}
